ac84b3d149d8ab71aa8d5d9a2a38c023f84ad4b7
Mateusz-11 <tweak.night@gmail.com>
2023-04-17 19:25:52 +0200

    Task 8 - Initial files

---
+++ part1-Introduction/8-clicker/app.js
@@ -0,0 +1,11 @@
+class Counter extends React.Component {
+	render() {
+		return;
+	}
+}
+
+const startValue = 0;
+ReactDOM.render(
+	<Counter result={startValue} />,
+	document.getElementById("root")
+);
