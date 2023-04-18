93d70a39bcc87888ab28b306dced2ca54068a659
Mateusz-11 <tweak.night@gmail.com>
2023-04-18 20:31:27 +0200

    Task 8 - Update

--- part1-Introduction/8-clicker/app.js
+++ part1-Introduction/8-clicker/app.js
@@ -1,6 +1,43 @@
 class Counter extends React.Component {
+	state = {
+		result: 0,
+		allClicks: 0,
+	};
+	handleClickUp = () => {
+		this.setState((prevState) => {
+			return {
+				result: prevState.result + 1,
+				allClicks: prevState.allClicks + 1,
+			};
+		});
+	};
+	handleClickDown = () => {
+		this.setState((prevState) => {
+			return {
+				result: prevState.result - 1,
+				allClicks: prevState.allClicks + 1,
+			};
+		});
+	};
+	handleClickReset = () => {
+		this.setState(() => {
+			return {
+				result: 0,
+			};
+		});
+	};
+
 	render() {
-		return;
+		// const { result } = this.state;
+		return (
+			<>
+				<button onClick={this.handleClickUp}>+1</button>
+				<button onClick={this.handleClickDown}>-1</button>
+				<button onClick={this.handleClickReset}>reset wyniku</button>
+				<h1>Liczba kliknięć: {this.state.allClicks}</h1>
+				<h2>Wynik: {this.state.result}</h2>
+			</>
+		);
 	}
 }
 
