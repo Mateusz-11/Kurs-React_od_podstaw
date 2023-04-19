4209715bf8904f318b959b664200c72fe7980003
Mateusz-11 <tweak.night@gmail.com>
2023-04-19 20:29:14 +0200

    Task 8 - Update

--- part1-Introduction/8-clicker/app.js
+++ part1-Introduction/8-clicker/app.js
@@ -3,39 +3,74 @@ class Counter extends React.Component {
 		result: 0,
 		allClicks: 0,
 	};
-	handleClickUp = () => {
-		this.setState((prevState) => {
-			return {
+	//  v1 - 3 functions
+	// handleClickUp = () => {
+	// 	this.setState((prevState) => {
+	// 		return {
+	// 			result: prevState.result + 1,
+	// 			allClicks: prevState.allClicks + 1,
+	// 		};
+	// 	});
+	// };
+	// handleClickDown = () => {
+	// 	this.setState((prevState) => {
+	// 		return {
+	// 			result: prevState.result - 1,
+	// 			allClicks: prevState.allClicks + 1,
+	// 		};
+	// 	});
+	// };
+	// handleClickReset = () => {
+	// 	this.setState(() => {
+	// 		return {
+	// 			result: 0,
+	// 		};
+	// 	});
+	// };
+	//  v2 - 1 function
+	handleClicks = (type, number) => {
+		if (type === "addition") {
+			this.setState((prevState) => ({
 				result: prevState.result + 1,
 				allClicks: prevState.allClicks + 1,
-			};
-		});
-	};
-	handleClickDown = () => {
-		this.setState((prevState) => {
-			return {
+			}));
+		} else if (type === "substraction") {
+			this.setState((prevState) => ({
 				result: prevState.result - 1,
 				allClicks: prevState.allClicks + 1,
-			};
-		});
-	};
-	handleClickReset = () => {
-		this.setState(() => {
-			return {
+			}));
+		} else if (type === "reset") {
+			this.setState((prevState) => ({
 				result: 0,
-			};
-		});
+				allClicks: prevState.allClicks + 1,
+			}));
+		}
 	};
 
 	render() {
 		// const { result } = this.state;
 		return (
 			<>
-				<button onClick={this.handleClickUp}>+1</button>
-				<button onClick={this.handleClickDown}>-1</button>
-				<button onClick={this.handleClickReset}>reset wyniku</button>
-				<h1>Liczba kliknięć: {this.state.allClicks}</h1>
-				<h2>Wynik: {this.state.result}</h2>
+				<div>
+					<button onClick={this.handleClicks.bind(this, "addition", 1)}>
+						+1
+					</button>
+					<button onClick={() => this.handleClicks("substraction", 1)}>
+						-1
+					</button>
+					<button onClick={() => this.handleClicks("reset")}>
+						reset wyniku
+					</button>
+					<h1>Liczba kliknięć: {this.state.allClicks}</h1>
+					<h2>Wynik: {this.state.result}</h2>
+				</div>
+				<div>
+					<button onClick={this.handleClickUp}>+1</button>
+					<button onClick={this.handleClickDown}>-1</button>
+					<button onClick={this.handleClickReset}>reset wyniku</button>
+					<h1>Liczba kliknięć: {this.state.allClicks}</h1>
+					<h2>Wynik: {this.state.result}</h2>
+				</div>
 			</>
 		);
 	}
