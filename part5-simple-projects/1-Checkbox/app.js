f3a1f8550dde2f6c13047bc7dbaf4989728e4663
Mateusz-11 <tweak.night@gmail.com>
2023-05-09 22:14:23 +0200

    Added html elements + simple Components

--- part5-simple-projects/1-Checkbox/app.js
+++ part5-simple-projects/1-Checkbox/app.js
@@ -1,9 +1,17 @@
+const PositiveMessage = () => <p>Możesz obejrzeć film. Zapraszamy</p>;
+const NegativeMessage = () => {
+	return <p>Nie możesz obejrzeć filmu.</p>;
+};
+
 class CheckboxAgeConfirmation extends React.Component {
 	render() {
 		console.log("isConfirmed");
 		return (
 			<>
 				<h1>Kup bilet na horror roku!</h1>
+				<input type='checkbox' />
+				<label htmlFor='age'>Mam co najmniej 16 lat</label>
+				{true ? <PositiveMessage /> : <NegativeMessage />}
 			</>
 		);
 	}
