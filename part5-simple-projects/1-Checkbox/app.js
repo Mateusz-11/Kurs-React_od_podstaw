fbbcad52c65a83359f3f999d71d3e36aa603145e
Mateusz-11 <tweak.night@gmail.com>
2023-05-12 19:45:40 +0200

    Changed display condition in function

--- part5-simple-projects/1-Checkbox/app.js
+++ part5-simple-projects/1-Checkbox/app.js
@@ -1,33 +1,49 @@
+// const { Button } = require("bootstrap");
+
 const PositiveMessage = () => <p>Możesz obejrzeć film. Zapraszamy</p>;
 const NegativeMessage = () => {
 	return <p>Nie możesz obejrzeć filmu.</p>;
 };
 
-class CheckboxAgeConfirmation extends React.Component {
+class TicketShop extends React.Component {
 	state = {
 		isChecked: true,
+		isFormSubmitted: false,
 	};
 	handleCheckboxChange = () => {
 		this.setState({
 			isChecked: !this.state.isChecked,
 		});
 	};
+	displayMessage = () => {
+		if (this.state.isFormSubmitted) {
+			if (this.state.isChecked) {
+				return <PositiveMessage />;
+			} else {
+				return <NegativeMessage />;
+			}
+		}
+	};
 	render() {
-		console.log("isConfirmed");
+		// console.log("isConfirmed");
 		return (
 			<>
 				<h1>Kup bilet na horror roku!</h1>
-				<input
-					type='checkbox'
-					id='age'
-					onChange={this.handleCheckboxChange}
-					checked={this.state.isChecked}
-				/>
-				<label htmlFor='age'>Mam co najmniej 16 lat</label>
-				{this.state.isChecked ? <PositiveMessage /> : <NegativeMessage />}
+				<form>
+					<input
+						type='checkbox'
+						id='age'
+						onChange={this.handleCheckboxChange}
+						checked={this.state.isChecked}
+					/>{" "}
+					<br />
+					<label htmlFor='age'>Mam co najmniej 16 lat</label>
+					<button type='submit'>Kup bilet</button>
+				</form>
+				{this.displayMessage()}
 			</>
 		);
 	}
 }
 
-ReactDOM.render(<CheckboxAgeConfirmation />, document.getElementById("root"));
+ReactDOM.render(<TicketShop />, document.getElementById("root"));
