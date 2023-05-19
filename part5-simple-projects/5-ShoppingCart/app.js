44bde4113cf14c0e1dfba1e10f6fc17ef458274c
Mateusz-11 <tweak.night@gmail.com>
2023-05-19 19:29:10 +0200

    Task 5-5 Initial files

--- part5-simple-projects/5-ShoppingCart/app.js
+++ part5-simple-projects/5-ShoppingCart/app.js
@@ -1,30 +1,9 @@
 class App extends React.Component {
-  state = {
-    availableProducts: 7,
-    shoppingCart: 1,
-  }
+	state = {};
 
-  handleRemoveFromCart = () => {
-    this.setState({
-      shoppingCart: this.state.shoppingCart - 1,
-    })
-  }
-
-  handleAddToCart = () => {
-    this.setState({
-      shoppingCart: this.state.shoppingCart + 1,
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <button disabled={this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
-        <span> {this.state.shoppingCart} </span>
-        <button disabled={this.state.shoppingCart === this.state.availableProducts ? true : false} onClick={this.handleAddToCart}>+</button>
-      </div>
-    )
-  }
+	render() {
+		return <div>x</div>;
+	}
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
