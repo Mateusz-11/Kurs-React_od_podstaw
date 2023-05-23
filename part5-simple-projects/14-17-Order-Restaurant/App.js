ebea31fdf42537ec351b881dc7f35b3ecced43db
Mateusz-11 <tweak.night@gmail.com>
2023-05-23 18:18:11 +0200

    Task 5-14 Initial files

--- part5-simple-projects/14-17-Order-Restaurant/App.js
+++ part5-simple-projects/14-17-Order-Restaurant/App.js
@@ -1,38 +1,26 @@
 class App extends React.Component {
-  state = {
-    items: [
-      { id: 1, name: "herbata", active: true },
-      { id: 2, name: "ziemniaki", active: false },
-      { id: 3, name: "kasza", active: false },
-      { id: 4, name: "zupa wodna", active: true },
-      { id: 5, name: "wrzątek", active: false },
-      { id: 6, name: "chleb", active: false },
-    ],
+	state = {
+		items: [
+			{ id: 1, name: "herbata", active: true },
+			{ id: 2, name: "ziemniaki", active: false },
+			{ id: 3, name: "kasza", active: false },
+			{ id: 4, name: "zupa wodna", active: true },
+			{ id: 5, name: "wrzątek", active: false },
+			{ id: 6, name: "chleb", active: false },
+		],
+	};
 
-  }
+	handleChangeStatus = (id) => {
+		console.log("działa");
+		console.log(id);
+	};
 
-  handleChangeStatus = (id) => {
-    // console.log("działa");
-    console.log(id);
-    const items = this.state.items.map(item => {
-      if (id === item.id) {
-        item.active = !item.active
-      }
-      return item
-    })
-
-    this.setState({
-      items
-    })
-
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <Header items={this.state.items} />
-        <ListItems items={this.state.items} changeStatus={this.handleChangeStatus} />
-      </React.Fragment>
-    );
-  }
+	render() {
+		return (
+			<React.Fragment>
+				<Header />
+				<ListItems />
+			</React.Fragment>
+		);
+	}
 }
