b29d18e0129cfa0949136640ada97c912bc7c8c3
Mateusz-11 <tweak.night@gmail.com>
2023-05-27 21:17:31 +0200

    Task 5-14 Fix code

--- part5-simple-projects/14-17-Order-Restaurant/ListItems.js
+++ part5-simple-projects/14-17-Order-Restaurant/ListItems.js
@@ -1,7 +1,14 @@
 const ListItems = (props) => {
 	const items = props.items.map((items) => (
-		<Item key={items.id} name={items.name} active={active} />
+		<Item
+			key={items.id}
+			id={items.id}
+			name={items.name}
+			active={items.active}
+			changeStatus={props.changeStatus}
+		/>
 	));
+
 	return (
 		<div className='list'>
 			<h1>Twoje zamówienie:</h1>
