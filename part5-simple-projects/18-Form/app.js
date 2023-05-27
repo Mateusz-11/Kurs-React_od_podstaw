74ad4804a56b83f9dfed85b65f48a28366b7a481
Mateusz-11 <tweak.night@gmail.com>
2023-05-27 22:10:10 +0200

    Task 5-18 Optimalization Form

--- part5-simple-projects/18-Form/app.js
+++ part5-simple-projects/18-Form/app.js
@@ -6,28 +6,18 @@ class Form extends React.Component {
 		number: "2",
 	};
 
-	handleCityChange = (e) => {
-		this.setState({
-			city: e.target.value,
-		});
+	handleChange = (e) => {
+		// console.log(e.target.name);
+		if (e.target.type === "checkbox") {
+			this.setState({
+				[e.target.name]: e.target.checked,
+			});
+		} else {
+			this.setState({
+				[e.target.name]: e.target.value,
+			});
+		}
 	};
-
-	handleTextChange = (e) => {
-		this.setState({
-			text: e.target.value,
-		});
-	};
-
-	handleIsLovedChange = (e) => {
-		this.setState({
-			isLoved: e.target.checked,
-		});
-	};
-	handleVisitNumberChange(e) {
-		this.setState({
-			number: e.target.value,
-		});
-	}
 
 	render() {
 		return (
@@ -35,8 +25,9 @@ class Form extends React.Component {
 				<label>
 					Podaj miasto
 					<input
+						name='city'
 						value={this.state.city}
-						onChange={this.handleCityChange}
+						onChange={this.handleChange}
 						type='text'
 					/>
 				</label>
@@ -44,24 +35,27 @@ class Form extends React.Component {
 				<label>
 					Napisz coś o tym mieście
 					<textarea
+						name='text'
 						value={this.state.text}
-						onChange={this.handleTextChange}></textarea>
+						onChange={this.handleChange}></textarea>
 				</label>
 				<br />
 				<label>
 					Czy lubisz to miasto?
 					<input
+						name='isLoved'
 						type='checkbox'
 						checked={this.state.isLoved}
-						onChange={this.handleIsLovedChange}
+						onChange={this.handleChange}
 					/>
 				</label>
 				<br />
 				<label>
 					Ile razy byłeś w tym mieście?
 					<select
+						name='number'
 						value={this.state.number}
-						onChange={this.handleVisitNumberChange.bind(this)}>
+						onChange={this.handleChange.bind(this)}>
 						<option value='0'>0</option>
 						<option value='1'>1</option>
 						<option value='2'>2</option>
