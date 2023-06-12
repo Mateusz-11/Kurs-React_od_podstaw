282b9b14e9f756b17e4da90e5baa6145f2f54e61
Mateusz-11 <tweak.night@gmail.com>
2023-06-12 21:32:04 +0200

    Draft

--- part5-simple-projects/25-zadanie2/app.js
+++ part5-simple-projects/25-zadanie2/app.js
@@ -1,4 +1,18 @@
 // https://websamuraj.pl/examples/react/zadanie-lista/
 // Wykorzystaj dwa komponenty: rodzica ( o nazwie List) i dziecka (o nazwie Person)
 
+class List extends React.Component {
+	state = {
+		people: [
+			{ id: 1, name: "Jan K." },
+			{ id: 2, name: "Piotr C." },
+			{ id: 3, name: "Marta K." },
+			{ id: 4, name: "Tomek O." },
+		],
+	};
+	render() {
+		return <div>apka</div>;
+	}
+}
+
 ReactDOM.render(<List />, document.getElementById("root"));
