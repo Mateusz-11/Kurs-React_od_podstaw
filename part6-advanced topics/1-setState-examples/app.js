bdd93880546ed5c0dd09decad7686886c4b7f1e8
Mateusz-11 <tweak.night@gmail.com>
2023-06-21 23:57:03 +0200

    draft

--- part6-advanced topics/1-setState-examples/app.js
+++ part6-advanced topics/1-setState-examples/app.js
@@ -18,6 +18,7 @@ class Clicker extends React.Component {
 				<button onClick={this.handleClick1}>Podbij o 1</button>
 				<h1>{this.state.number1}</h1>
 				<button onClick={this.handleClick2}>Podbij o 3</button>
+				<button onClick={this.handleClick2}>Podbij o 3</button>
 				<h1>{this.state.number2}</h1>
 				<button onClick={this.handleClick3}>Podbij o 3</button>
 				<h1>{this.state.number3}</h1>
