3d1f025b4e1733d02610520fa798f971153054ba
Mateusz-11 <tweak.night@gmail.com>
2023-06-24 20:41:22 +0200

    Initial files

--- part6-advanced topics/2-lifecycle-metody/app.js
+++ part6-advanced topics/2-lifecycle-metody/app.js
@@ -1,60 +1,53 @@
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      number: 0
-    }
-    console.log("Wywołać metodę constructor");
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			number: 0,
+		};
+		// console.log("Wywołać metodę constructor");
+	}
 
-  componentWillMount() {
-    console.log('Wywołana componentWillMount');
-  }
+	componentWillMount() {
+		// console.log('Wywołana componentWillMount');
+	}
 
-  componentDidMount() {
-    console.log('Wywołana componentDidMount');
-    this.setState({
-      number: 1
-    })
-  }
+	componentDidMount() {
+		// console.log('Wywołana componentDidMount');
+	}
 
-  componentDidUpdate() {
-    console.log('Wywołana componentDidUpdate');
-  }
+	componentDidUpdate() {
+		// console.log('Wywołana componentDidUpdate');
+	}
 
-  render() {
-    console.log("Wywołana metoda render");
+	render() {
+		// console.log("Wywołana metoda render");
 
-    return (
-      <div>
-        <p>
-          lifecycle - montowanie komponentu
-      </p>
-        <Child />
-      </div>
-    )
-  }
-
-
+		return (
+			<div>
+				<p>lifecycle - montowanie komponentu</p>
+				{/* <Child /> */}
+			</div>
+		);
+	}
 }
 
-class Child extends React.Component {
-  componentDidMount() {
-    console.log('Wywołana componentDidMount w komponencie Child');
-    // this.setState({
-    //   number: 1
-    // })
-  }
+// class Child extends React.Component {
+//   componentDidMount() {
+//     console.log('Wywołana componentDidMount w komponencie Child');
+//     // this.setState({
+//     //   number: 1
+//     // })
+//   }
 
-  render() {
-    console.log(`
-    -----------------------------
-    Wywołana metoda render w komponencie Child
-    `);
-    return (
-      <h1>Dziecko</h1>
-    )
-  }
-}
+//   render() {
+//     console.log(`
+//     -----------------------------
+//     Wywołana metoda render w komponencie Child
+//     `);
+//     return (
+//       <h1>Dziecko</h1>
+//     )
+//   }
+// }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
