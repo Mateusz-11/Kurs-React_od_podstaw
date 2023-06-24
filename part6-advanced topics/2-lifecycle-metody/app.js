00db37c9155b3ad665d81ebc262666839c36778b
Mateusz-11 <tweak.night@gmail.com>
2023-06-24 21:01:06 +0200

    End of example

--- part6-advanced topics/2-lifecycle-metody/app.js
+++ part6-advanced topics/2-lifecycle-metody/app.js
@@ -4,50 +4,53 @@ class App extends React.Component {
 		this.state = {
 			number: 0,
 		};
-		// console.log("Wywołać metodę constructor");
+		console.log("Wywołać metodę constructor");
 	}
 
 	componentWillMount() {
-		// console.log('Wywołana componentWillMount');
+		console.log("Wywołana componentWillMount");
 	}
 
 	componentDidMount() {
-		// console.log('Wywołana componentDidMount');
+		console.log("Wywołana componentDidMount");
+		this.setState({
+			number: 1,
+		});
 	}
 
 	componentDidUpdate() {
-		// console.log('Wywołana componentDidUpdate');
+		console.log("Wywołana componentDidUpdate");
 	}
 
 	render() {
-		// console.log("Wywołana metoda render");
+		console.log("Wywołana metoda render");
 
 		return (
 			<div>
 				<p>lifecycle - montowanie komponentu</p>
-				{/* <Child /> */}
+				<Child />
 			</div>
 		);
 	}
 }
 
-// class Child extends React.Component {
-//   componentDidMount() {
-//     console.log('Wywołana componentDidMount w komponencie Child');
-//     // this.setState({
-//     //   number: 1
-//     // })
-//   }
+class Child extends React.Component {
+  componentDidMount() {
+    console.log('Wywołana componentDidMount w komponencie Child');
+    // this.setState({
+    //   number: 1
+    // })
+  }
 
-//   render() {
-//     console.log(`
-//     -----------------------------
-//     Wywołana metoda render w komponencie Child
-//     `);
-//     return (
-//       <h1>Dziecko</h1>
-//     )
-//   }
-// }
+  render() {
+    console.log(`
+    -----------------------------
+    Wywołana metoda render w komponencie Child
+    `);
+    return (
+      <h1>Dziecko</h1>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
